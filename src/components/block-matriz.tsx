edc8485958a6846c7596fe57ed494e9dac68b2b3
Jonatan Iparraguirre <jonatan@iparraguirre.pro>
2026-01-28 14:44:01 -0500

    feat: form interaction and input validation

--- src/components/block-matriz.tsx
+++ src/components/block-matriz.tsx
@@ -2,12 +2,31 @@
 
 import { useEffect, useState } from "react"
 import { Loader2 } from "lucide-react"
+import { useForm } from "react-hook-form";
+
 import BlockMatrizResultComponent from "./block-matriz-result"
+import { FormMatriz } from "@/app/types/Matriz";
+import { FORM_MESSAGES } from "@/lib/messages";
+
 
 export default function BlockMatrizComponent() {
   const [isClient, setIsClient] = useState(false)
   const iMatriz = [[4, 5, 3, 4,], [6, 7, 3, 4]];
   const isLarge = iMatriz.length && iMatriz[0]?.length > 8;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormMatriz>(
+    {
+      mode: 'onChange'
+    }
+  );
+
+  const doSubmit = (data: FormMatriz) => {
+    console.log(data);
+  };
 
   useEffect(() => {
     setIsClient(true)
@@ -23,14 +42,35 @@ export default function BlockMatrizComponent() {
 
   return (
     <div className="py-5">
-      <div>
+      <form onSubmit={handleSubmit(doSubmit)}>
         <label className="text-black font-semibold text-sm" htmlFor="">
           Matriz
-          <textarea rows={3} className="w-full px-2 py-3 border border-gray-400 bg-white rounded" placeholder="Ingresa la matriz" />
-        </label>
-      </div>
+          <textarea rows={3}
+            {...register("matriz", {
+              validate: (value: any) => {
+                try {
+                  const parsed = JSON.parse(value);
+                  const isValid = Array.isArray(parsed) && parsed.filter(
+                    (inner: any) =>
+                      Array.isArray(inner) &&
+                      inner.filter((num) => typeof num === "number").length === inner.length
+                  ).length === parsed.length
 
-      <div className={`flex my-2 justify-between ${isLarge && 'flex-col'}`}>
+                  return isValid || FORM_MESSAGES.inputError
+                } catch {
+                  return FORM_MESSAGES.inputError;
+                }
+              },
+            })}
+            className={`w-full px-2 py-3 border border-gray-400  rounded ${errors.matriz ? 'bg-red-100' : 'bg-white'}`}
+            placeholder="Por ejemplo: [ [1,2], [3,4] ]" />
+          {errors.matriz && <p className="mb-4 text-xs text-red-500">{errors.matriz.message}</p>}
+        </label>
+
+        <button type="submit" disabled={!!errors.matriz} className={`px-3 py-2 bg-black text-white text-sm font-semibold ${!!errors.matriz && 'opacity-50'}`}>Procesar Matriz</button>
+      </form>
+
+      <div className={`flex mt-5 mb-2 justify-between ${isLarge && 'flex-col'}`}>
         <div className={`min-h-[100px] p-2 bg-gray-100 ${isLarge ? 'w-full' : 'w-1/2'}`}>
           <p className="text-sm text-black font-semibold">Input:</p>
           <div className="overflow-hidden">
---
+++ src/lib/messages.ts
@@ -0,0 +1,4 @@
+export const FORM_MESSAGES = {
+  inputTypeError: "Debes ingresar una matriz valida.",
+  inputError: "Debes ingresar un campo parecido a este: [ [1,2], [3,4] ].",
+};
